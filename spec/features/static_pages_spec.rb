fd75be87b9c0d5f610140f3afbbd3fb886ce2b19
kostja <konstantin@silin.info>
2013-04-10 08:30:26 +0200

    added about page and feature specs for static pages

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+
+    it "should have a welcome message" do
+      # equivalent to '/static_pages/home'
+      visit static_pages_home_path
+      page.should have_content "Welcome"
+    end
+
+    it "should have it's name in the title" do
+      # equivalent to '/static_pages/home'
+      visit static_pages_home_path
+      page.source.should have_selector('title', text: "Mooi | Welcome")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have it's name rendered" do
+      # equivalent to '/static_pages/help'
+      visit static_pages_help_path
+      page.should have_content "Mooi | Help"
+    end
+
+    it "should have it's name in the title" do
+      # equivalent to '/static_pages/help'
+      visit static_pages_help_path
+      page.source.should have_selector('title', text: "Mooi | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have it's name rendered" do
+      # equivalent to '/static_pages/help'
+      visit static_pages_about_path
+      p page.document.text
+      page.should have_content "Mooi | About"
+    end
+
+    it "should have it's name in the title" do
+      # equivalent to '/static_pages/help'
+      visit static_pages_about_path
+      # due to a change in capybara, checking the 'page' is restricted to visible elements only.
+      # for incisible elements like 'title', use 'page.source'
+      page.source.should have_selector('title', text: "Mooi | About")
+    end
+
+  end
+
+end
