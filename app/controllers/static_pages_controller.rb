0afea74acc9d1f2d5804d1a993e2082ec1dd6186
kostja <konstantin@silin.info>
2013-04-10 08:29:48 +0200

    added full_title helper for views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Welcome"
   end
 
   def help
+  @title = "Help"
+  end
+
+  def about
+    @title = "About this application"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    global_title = "Mooi"
+    if(page_title.empty?)
+      global_title
+    end
+      "#{global_title} | #{page_title}"
+  end
 end
